61ff236a1304d8c66b608626f7cbb3b3c05bbe61
Thodoris Greasidis <thgreasi@gmail.com>
2020-10-07 18:12:53 +0300

    Stop generating a browser build
    Change-type: major

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,16 +1,10 @@
 import * as fetchPonyfillFactory from 'fetch-ponyfill';
-import PineFetchNode from '../src/index';
-// tslint:disable-next-line:no-var-requires
-const { default: PineFetchBrowser } = require('../build/index.umd');
+import PineFetch from '../src/index';
 
 const API_BASE_URL = 'https://api.balena-cloud.com/';
 const API_VERSION = 'v5/';
 
 const IS_BROWSER = typeof window !== 'undefined';
-
-const PineFetch: typeof PineFetchNode = IS_BROWSER
-	? PineFetchBrowser
-	: PineFetchNode;
 
 // While testing on a browser, rely on the library to use the native `fetch`.
 const backendParams = IS_BROWSER
@@ -19,7 +13,7 @@ const backendParams = IS_BROWSER
 
 export interface TestSuiteContext extends Mocha.Context {
 	// So that TS lets us type the `this` of `it` calls.
-	pineClient?: PineFetchNode;
+	pineClient?: PineFetch;
 }
 
 export const givenAPineClient = function (beforeFn: Mocha.HookFunction) {
