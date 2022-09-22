4ac3f76e1628e7dd705fa23399031eddb4b9558b
Thodoris Greasidis <thgreasi@gmail.com>
2022-09-22 16:00:20 +0300

    Replace balenaCI with flowzone
    Change-type: patch

--- karma.conf.js
+++ karma.conf.js
@@ -4,5 +4,9 @@ const packageJSON = require('./package.json');
 module.exports = (config) => {
 	const karmaConfig = getKarmaConfig(packageJSON);
 	karmaConfig.logLevel = config.LOG_INFO;
+	karmaConfig.webpack.experiments = {
+		asyncWebAssembly: true,
+	};
+
 	config.set(karmaConfig);
 };
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,8 +1,8 @@
 import * as chai from 'chai';
+import { expect } from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 chai.use(chaiAsPromised);
 
-import { expect } from './chai';
 import { givenAPineClient, TestSuiteContext } from './setup';
 import { AnyObject } from './common';
 
